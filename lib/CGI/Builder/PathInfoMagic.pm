e657ed382cb55f0d5537648568cd1f1e8d6e2006
Tomohiro Teranishi <tomyhero@cpan.org>
2006-07-12 11:37:10 +0000

    Import of TOMYHERO/CGI-Builder-PathInfoMagic-0.03 from CPAN.
    gitpan-cpan-distribution: CGI-Builder-PathInfoMagic
    gitpan-cpan-version:      0.03
    gitpan-cpan-path:         TOMYHERO/CGI-Builder-PathInfoMagic-0.03.tar.gz
    gitpan-cpan-author:       TOMYHERO
    gitpan-cpan-maturity:     released

--- lib/CGI/Builder/PathInfoMagic.pm
+++ lib/CGI/Builder/PathInfoMagic.pm
@@ -3,7 +3,7 @@ package CGI::Builder::PathInfoMagic;
 use strict;
 use base qw/CGI::Builder/;
 use vars qw/$VERSION/;
-$VERSION = '0.02';
+$VERSION = '0.03';
 
 sub process {
     my $s  = shift;
@@ -22,76 +22,15 @@ sub __path_info_magic{
 
 =head1 NAME
 
-CGI::Builder::PathInfoMagic - Get page name from PAHT_INFO instead of p
+CGI::Builder::PathInfoMagic - Deprecated. Use CGI::Builder::GetPageName instead.
 
 =head1 DESCRIPTION
 
-This module allow you to set page name from $ENV{PATH_INFO} instead of
-p(Qerystring). You can check SYNOPSYS out and you will know what this mean. :-) 
-
-This class inherits from CGI::Builder, I love CGI::Builder.
-
-=head1 SYNOPSYS
-
-start.cgi
-
- #!/usr/bin/perl -w
-
- use strict;
- use Your::CGI::Builder;
- 
- my $app = Your::CGI::Builder->new();
- $app->process();
-
- __END__
-
-Your CGI::Builder Package.
-
- package Your::CGI::Builder;
- 
- use CGI::Builder::PathInfoMagic qw/
-    CGI::Builder::DFVCheck
- /;
- 
- sub PH_foo_bar {
-    my $s = shift;
-    $s->page_content( 'my URL is http://localhost/script.cgi/foo/bar/?foo=whatever !!!!' );
- }
- 
- sub PH_hoge {
-    my $s = shift;
-    $s->page_content = 'my URL is http://localhost/script.cgi/hoge/ !!!' ;
- }
-
-
-=head1 MORE FUN?
-
-Use ScriptAlias !!! This allow you to hide .cgi extension. Very fun.
-
-
- ScriptAlias /secure /var/www/httpdoc/secure.cgi
-
- # You have this start script.
- http://localhost/secure.cgi 
-
- # You set script alias so , you can also access with this URL.
- http://localhost/secure
- 
- # Then now...
- sub PH_foo_bar {
-    my $s = shift;
-    $s->page_content = 'my URL is http://localhost/secure/foo/bar/?foo=whatever !!!' ;
- }
-
-=head1 Override METHOD
-
-=head2 process()
-
- You can not set any aregument because I will set it for you . :-)
+This module is deprecated. please use CGI::Builder::GetPageName istead.  Thanks.
 
 =head1 SEE ALSO
 
-CGI::Builder
+CGI::Builder::GetPageName
 
 =head1 AUTHOR
 
